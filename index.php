30f8d8b328407ab1ba4c31f97287a2c6d8f889a6
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-10-03 04:39:52 +0900

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file replaces the concrete/dispatcher.php to bootstrap the CMS for
+ * use as a dependancy.
+ */
+/*
+ * ----------------------------------------------------------------------------
+ * Set our own version of __DIR__ as $__DIR__ so we can include this file on
+ * PHP < 5.3 and have it not die wholesale.
+ * ----------------------------------------------------------------------------
+ */
+$__DIR__ = 'vendor/concrete5/concrete5';
+/*
+ * ----------------------------------------------------------------------------
+ * Override some of the concrete5 dcore directory location
+ * ----------------------------------------------------------------------------
+ */
+defined( 'DIRNAME_CORE' ) or define( 'DIRNAME_CORE', $__DIR__ . DIRECTORY_SEPARATOR . 'concrete' );
+/*
+ * ----------------------------------------------------------------------------
+ * Add the vendor path to the list of include paths
+ * ----------------------------------------------------------------------------
+ */
+ini_set('include_path', __DIR__. DIRECTORY_SEPARATOR. 'vendor' . PATH_SEPARATOR . get_include_path());
+/*
+ * ----------------------------------------------------------------------------
+ * Require the composer autoloaders
+ * ----------------------------------------------------------------------------
+ */
+require 'vendor/autoload.php';
+
+require $__DIR__ . DIRECTORY_SEPARATOR . 'concrete/dispatcher.php';
